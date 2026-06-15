d0dee97e8b010bb11d194afc189deef0e5c11ce3
Varunappalla1993 <an.varun1993@gmail.com>
2026-06-15 15:30:50 +0530

    shell1

--- ShellScripts/1_HelloWorld.sh
+++ ShellScripts/1_HelloWorld.sh
@@ -7,3 +7,8 @@ echo "HEllO SHELL WORLD"
 NAME=VARUN
 AGE=32
 echo HELLO MY NAME IS ${NAME} and I am $AGE YEARS OLD
+
+# pass args via terminal
+NAAM=$1
+UMAR=$2
+echo HELLO MY NAME IS ${NAAM} and I am ${UMAR} YEARS OLD

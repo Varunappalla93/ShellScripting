29f6ecd0b9097159a2209dec4beefc1d0e90ffc6
Varunappalla1993 <an.varun1993@gmail.com>
2026-06-15 16:00:00 +0530

    shell2

--- ShellScripts/2_Shell.sh
+++ ShellScripts/2_Shell.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Special Variables
-echo "All args passed to script - $@"  # All args passed to script - 1 2 3 4 5 99
+echo "All args passed to script - $@" 
 echo "No of vars passed to script - $#"
 echo "Script Name - $0"
 echo "present directory - $PWD"
 echo "who is running script - $USER"
 echo "Home directory - $HOME"
 echo "PID of script - $$"
+sleep 100 & 
 echo "To access background process id - $!" # eg: sleep 100 & 
+
+# eg:
+# sh 2_Shell.sh 1 2 3 4 5 99
+# All args passed to script - 1 2 3 4 5 99
+# No of vars passed to script - 6
+# Script Name - 2_Shell.sh
+# present directory - /home/ec2-user/ShellScripting/ShellScripts
+# who is running script - ec2-user
+# Home directory - /home/ec2-user
+# PID of script - 2076
+# To access background process id - 

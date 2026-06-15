94004a2e33dad03eaa714a2de7f687ca483f0cac
Varunappalla1993 <an.varun1993@gmail.com>
2026-06-15 15:56:38 +0530

    shell2

--- ShellScripts/2_Shell.sh
+++ ShellScripts/2_Shell.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Special Variables
-# $1 $2...$N - args passed
-echo "All args passed to script - $@"
+echo "All args passed to script - $@"  # All args passed to script - 1 2 3 4 5 99
 # No of args passed to script - $#
 # Script Name - $0
 # present directory - $PWD
@@ -10,4 +9,3 @@ echo "All args passed to script - $@"
 # Home directory - $HOME
 # PID of script - $$
 # $! - to access background process id, eg: sleep 100 & 
-
